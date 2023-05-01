f7922cea6248df6b34bf48bdddf358ba272db91c
Syed-Hamza-Ali-Shah-0C <syedhamza.oc@gmail.com>
2023-05-01 23:30:17 +0500

    task-2 : Implemented search

--- todo-list-frontend/src/app/app.component.ts
+++ todo-list-frontend/src/app/app.component.ts
@@ -12,9 +12,10 @@ import {Observable} from "rxjs";
     </div>
     <div class="list">
       <label for="search">Search...</label>
-      <input id="search" type="text">
+      <input id="search" type="text" [(ngModel)]="searchTerm" (input)="filterTodos()" />
       <app-progress-bar *ngIf="!loaded"></app-progress-bar>
-      <app-todo-item *ngFor="let todo of todos$ | async" [item]="todo"></app-todo-item>
+      <app-todo-item *ngFor="let todo of filteredTodos" [item]="todo"></app-todo-item>
+
     </div>
   `,
   styleUrls: ['app.component.scss']
@@ -23,11 +24,23 @@ export class AppComponent {
 
   readonly todos$: Observable<Todo[]>;
   loaded = false;
+  todos: Todo[] = [];
+  filteredTodos: Todo[] = [];
+  searchTerm = '';
 
   constructor(todoService: TodoService) {
     this.todos$ = todoService.getAll();
-    this.todos$.subscribe(() => {
+    this.todos$.subscribe((todos) => {
+      this.todos = todos;
+      this.filteredTodos = this.todos;
       this.loaded = true;
     });
+  }
+
+  filterTodos() {
+    const searchTerm = this.searchTerm.toLowerCase();
+    this.filteredTodos = this.todos.filter(todo =>
+        todo.task.toLowerCase().includes(searchTerm)
+    );
   }
 }
